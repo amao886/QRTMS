a164ed7a605802b426253c973a30797627c6a20d
gw_djq <junqiangdang@163.com>
2019-09-08 10:40:48 +0800

    SQL添加

--- db/5.0.9.sql
+++ db/5.0.9.sql
@@ -3,3 +3,5 @@ ALTER TABLE `waybill_tab` ADD COLUMN `load_no` VARCHAR(20) DEFAULT ""  COMMENT '
 ALTER TABLE `waybill_tab` ADD COLUMN `car_type` VARCHAR(150) DEFAULT ""  COMMENT '车型'; 
 ALTER TABLE `waybill_tab` ADD COLUMN `distance` DECIMAL(10,2) DEFAULT 0.00  COMMENT '最新位置与目的地的距离';
 ALTER TABLE `waybill_tab` ADD COLUMN `load_time` VARCHAR(50) DEFAULT ""  COMMENT '装运日期';
+ALTER TABLE `waybill_tab` ADD COLUMN `pre_distance` DECIMAL(10,2) DEFAULT 0.00 COMMENT '上一次位置与目的地的距离';
+ALTER TABLE `driver_track_tab` ADD COLUMN `barcode` varchar(20) DEFAULT '' COMMENT '条码编号',
