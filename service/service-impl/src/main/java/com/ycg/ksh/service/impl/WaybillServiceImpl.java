b01147c60b08a97c5ad71ad5931c2f69173b465d
gw_djq <junqiangdang@163.com>
2019-09-09 09:39:34 +0800

    任务单上一次距离代码提交

--- service/service-impl/src/main/java/com/ycg/ksh/service/impl/WaybillServiceImpl.java
+++ service/service-impl/src/main/java/com/ycg/ksh/service/impl/WaybillServiceImpl.java
@@ -1560,12 +1560,13 @@ public class WaybillServiceImpl implements WaybillService, ReceiptObserverAdapte
             File destFile = FileUtils.newFile(fileEntity.getDirectory(), fileEntity.getFileName());
             easyExcel = EasyExcelBuilder.createWriteExcel(destFile);
             easyExcel.createSheet("出库单列表");
-            easyExcel.columnWidth(30,35, 30, 30,35, 20, 20, 10, 10, 20, 20, 30, 35,10);
-            easyExcel.header("提货日期","经销商简称","始发城市", "目的城市","配送地址", "装运号", "送货单号", "数量", "体积", "车型", "预计到达","实际到达日期","当前位置","距离目的地剩余（km）");
+            easyExcel.columnWidth(30,40, 30, 30,40, 20, 20, 10, 10, 20, 20, 30, 35, 10, 10);
+            easyExcel.header("提货日期","经销商简称","始发城市", "目的城市","配送地址", "装运号", "送货单号", "数量", "体积", "车型", "预计到达","实际到达日期","当前位置","距离目的地剩余（km）","上一次距离目的地剩余（km）");
             for (Waybill waybill : depotAlliances) {
                 easyExcel.row(StringUtils.isNotBlank(waybill.getLoadTime()) ? waybill.getLoadTime() : "",waybill.getReceiverName(),StringUtils.isNotBlank(waybill.getSimpleStartStation())?waybill.getSimpleStartStation():"",waybill.getSimpleEndStation(),
                         StringUtils.isNotBlank(waybill.getReceiveAddress())? waybill.getReceiveAddress():"",StringUtils.isNotBlank(waybill.getLoadNo())? waybill.getLoadNo():"",StringUtils.isNotBlank(waybill.getDeliveryNumber()) ? waybill.getDeliveryNumber() :"",waybill.getNumber(),waybill.getVolume()==null?0:waybill.getVolume(),
-                        StringUtils.isNotBlank(waybill.getCarType()) ? waybill.getCarType() : "",waybill.getArrivaltime() != null ? DateUtils.formatDate(waybill.getArrivaltime()) : "",waybill.getActualArrivalTime()!= null ? DateUtils.formatDate(waybill.getActualArrivalTime()):"",StringUtils.isNotBlank(waybill.getAddress()) ? waybill.getAddress() : "", waybill.getDistance()!=null?waybill.getDistance():"");
+                        StringUtils.isNotBlank(waybill.getCarType()) ? waybill.getCarType() : "",waybill.getArrivaltime() != null ? DateUtils.formatDate(waybill.getArrivaltime()) : "",waybill.getActualArrivalTime()!= null ? DateUtils.formatDate(waybill.getActualArrivalTime()):"",StringUtils.isNotBlank(waybill.getAddress()) ? waybill.getAddress() : "", 
+                                waybill.getDistance()!=null?waybill.getDistance():"",waybill.getPreDistance()!=null?waybill.getPreDistance():"");
             }
             easyExcel.write();
             fileEntity.setPath(destFile.getPath());
